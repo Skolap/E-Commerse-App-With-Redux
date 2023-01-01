ce5dc0ae3b63fd67ff4e649c965460b2480004cd
Shailesh Kolap <skolap22@gmail.com>
2023-01-01 12:22:31 +0530

    Added Badge

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import Badge from "@mui/material/Badge";
 
 const Header = () => {
   return (
@@ -12,10 +13,12 @@ const Header = () => {
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
           </Nav>
-          <i
-            class="fa-solid fa-cart-shopping text-light"
-            style={{ fontSize: 25, cursor: "pointer" }}
-          ></i>
+          <Badge badgeContent={4} color="primary">
+            <i
+              class="fa-solid fa-cart-shopping text-light"
+              style={{ fontSize: 25, cursor: "pointer" }}
+            ></i>
+          </Badge>
         </Container>
       </Navbar>
     </>
